f8afb3a81fcef809475f9759cd323ec22ba87a2d
Jeff Byrnes <jeff@evertrue.com>
2013-09-24 12:17:44 -0400

    Change maintainer email to reflect reality

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'et_www'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
+maintainer_email 'jeff@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_www'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
