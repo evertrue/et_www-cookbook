33a9d9595ed507604222968b7b4bf3e83aaeb16a
Jeff Byrnes <jeff@evertrue.com>
2014-09-30 18:34:48 -0400

    Completely disable New Relic app monitoring

--- attributes/newrelic.rb
+++ attributes/newrelic.rb
@@ -2,7 +2,7 @@ nr_license = Chef::EncryptedDataBagItem.load('secrets', 'api_keys')['newrelic']
 set['newrelic']['license'] = nr_license
 set['newrelic_meetme_plugin']['license'] = nr_license
 set['newrelic']['application_monitoring']['app_name'] = 'www'
-set['newrelic']['application_monitoring']['enabled'] = true
+set['newrelic']['application_monitoring']['enabled'] = false
 set['newrelic']['php_agent']['config_file'] = "#{node['php']['ext_conf_dir']}/newrelic.ini"
 
 # Plugin Agent settings for Apache & APC monitoring
